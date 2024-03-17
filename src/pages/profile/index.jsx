d6b49f2734962057f95fd07aae01a87b056a1926
Yohanes <yohanesdahlan@gmail.com>
2024-03-17 20:35:57 +0700

    slicing profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -5,6 +5,7 @@ import edit from "../../assets/img/edit.png";
 import { Button, Nav, Tab } from "react-bootstrap";
 import recipe1 from "../../assets/img/recipe10.png";
 import recipe2 from "../../assets/img/recipe11.png";
+import Footer from "../../components/Footer";
 
 const Profile = () => {
   const [activeTab, setActiveTab] = useState("link-1");
@@ -45,26 +46,59 @@ const Profile = () => {
                 <h3>Halaman My Recipe</h3>
                 <div className="row">
                   <div className="col-4">
-                    <div className="card" style={{ width: "370px", height: "250" }}>
+                    <div className="card" style={{ width: "370px", height: "250px" }}>
                       <img src={recipe1} alt="recipe1" />
+                      <h4 style={{ zIndex: "1", position: "absolute", marginTop: "150px", marginLeft: "25px", color: "#FFF5EC", fontSize: "28px", fontWeight: "500" }}>Bomb Chicken</h4>
                     </div>
                   </div>
                   <div className="col-4">
-                    <div className="card" style={{ width: "370px", height: "250" }}>
+                    <div className="card" style={{ width: "370px", height: "250px" }}>
                       <img src={recipe2} alt="recipe2" />
+                      <h4 style={{ zIndex: "1", position: "absolute", marginTop: "150px", marginLeft: "25px", color: "#FFF5EC", fontSize: "28px", fontWeight: "500" }}>Bananas Pancake</h4>
                     </div>
                   </div>
                 </div>
               </Tab.Pane>
               <Tab.Pane eventKey="link-2">
                 <h3>Halaman Saved Recipe</h3>
+                <div className="row">
+                  <div className="col-4">
+                    <div className="card" style={{ width: "370px", height: "250px" }}>
+                      <img src={recipe1} alt="recipe1" />
+                      <h4 style={{ zIndex: "1", position: "absolute", marginTop: "150px", marginLeft: "25px", color: "#FFF5EC", fontSize: "28px", fontWeight: "500" }}>Bomb Chicken</h4>
+                    </div>
+                  </div>
+                  <div className="col-4">
+                    <div className="card" style={{ width: "370px", height: "250px" }}>
+                      <img src={recipe2} alt="recipe2" />
+                      <h4 style={{ zIndex: "1", position: "absolute", marginTop: "150px", marginLeft: "25px", color: "#FFF5EC", fontSize: "28px", fontWeight: "500" }}>Bananas Pancake</h4>
+                    </div>
+                  </div>
+                </div>
               </Tab.Pane>
               <Tab.Pane eventKey="link-3">
                 <h3>Halaman Liked Recipe</h3>
+                <div className="row">
+                  <div className="col-4">
+                    <div className="card" style={{ width: "370px", height: "250px" }}>
+                      <img src={recipe1} alt="recipe1" />
+                      <h4 style={{ zIndex: "1", position: "absolute", marginTop: "150px", marginLeft: "25px", color: "#FFF5EC", fontSize: "28px", fontWeight: "500" }}>Bomb Chicken</h4>
+                    </div>
+                  </div>
+                  <div className="col-4">
+                    <div className="card" style={{ width: "370px", height: "250px" }}>
+                      <img src={recipe2} alt="recipe2" />
+                      <h4 style={{ zIndex: "1", position: "absolute", marginTop: "150px", marginLeft: "25px", color: "#FFF5EC", fontSize: "28px", fontWeight: "500" }}>Bananas Pancake</h4>
+                    </div>
+                  </div>
+                </div>
               </Tab.Pane>
             </Tab.Content>
           </div>
         </Tab.Container>
+      </div>
+      <div style={{ marginTop: "65px" }}>
+        <Footer />
       </div>
     </div>
   );
